bfb511aba9fb32f70ae32ccbe92e1a813baa1a13
Niena <58356541+ChoiSukwoo@users.noreply.github.com>
2021-11-16 00:29:28 +0900

    헤더완성
    웹 태블릿 모바일 헤더완성
    모바일 같은경우 메뉴창겸함

--- src/Layout/MainLayout.js
+++ src/Layout/MainLayout.js
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import Header from './Header';
 import Content from './Content';
 
+import Planet from '../Resource/data.json'
+import bgStar from '../Resource/background-stars.svg'
+
 const View = styled.div`
+    width: 100%; height: 100%;
     display:flex; flex-direction: column;
+    background-color: #070724;  background-image: url(${props => props.bgSvg});
 `
 
 const useResize = () => {
@@ -27,7 +32,7 @@ const useResize = () => {
             nowDevice = 'web'
         }
 
-        console.log("width : ", window.innerWidth, " height : ", window.innerHeight," device : ",nowDevice);
+        console.log("width : ", window.innerWidth, " height : ", window.innerHeight, " device : ", nowDevice);
 
         setState({
             x: window.scrollX,
@@ -47,17 +52,56 @@ const useResize = () => {
     return state.device;
 };
 
+const useHeaderOpen = () => {
+
+    const [state, setState] = useState('close');
+
+    useEffect(() => {
+    }, []);
+
+    return {
+        header: state,
+        setheader: setState
+    };
+
+};
+
+const usePlanet = () => {
+
+    const planetInfo = Planet.map((value, key) => ({name:value.name, color:value.color}))
+
+
+    const [state, setState] = useState('close');
+
+    const setPlanet = () =>{
+
+    }
+
+
+    useEffect(() => {
+    }, []);
+
+    return {
+        planetInfo:planetInfo,
+        planet: state,
+        setplanet: setState
+    };
+}
+
+
+
 function MainLayout() {
 
     const device = useResize()
+    const headerState = useHeaderOpen()
+    const planet = usePlanet()
 
-  return (
-    <View>
-        <Header></Header>
-        <Content></Content>
-        {device}
-    </View>
-  );
+    return (
+        <View bgSvg={bgStar}>
+            <Header Device={device} HeaderState={headerState} PlanetInfo={planet.planetInfo}></Header>
+            <Content></Content>
+        </View>
+    );
 }
 
 export default MainLayout;
